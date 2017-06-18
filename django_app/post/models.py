3b3484dec91ae475478b602e9b0fc9906a3fa60a
Gihong Kim <gyong1211@gmail.com>
2017-06-18 17:45:52 +0900

    post app 추가(Post, PostLike, Comment, CommentLike, Tag model 생성)

---
+++ django_app/post/models.py
@@ -0,0 +1,94 @@
+from django.conf import settings
+from django.db import models
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    photo = models.ImageField()
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        through='PostLike',
+        related_name='like_posts'
+    )
+    created_date = models.DateTimeField(
+        auto_now_add=True,
+    )
+    modified_date = models.DateTimeField(
+        auto_now=True,
+    )
+    tag = models.ManyToManyField(
+        'Tag',
+        blank=True
+    )
+
+    def __str__(self):
+        return "{}'s Post".format(self.author.username)
+
+    def add_comment(self, user, content):
+        return self.comment_set.create(
+            author=user,
+            content=content,
+        )
+
+    def add_tag(self, tag_name):
+        tag, tag_name = Tag.objects.get_or_create(name=tag_name)
+        if tag not in self.tag_set.filter(name=tag_name):
+            self.tag_set.add(tag)
+
+    @property
+    def like_count(self):
+        return self.like_users.count()
+
+
+class PostLike(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+    )
+    created_date = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+    )
+    content = models.CharField(
+        max_length=100,
+    )
+    created_date = models.DateTimeField(
+        auto_now_add=True,
+    )
+    modified_date = models.DateTimeField(
+        auto_now=True,
+    )
+
+
+class CommentLike(models.Model):
+    comment = models.ForeignKey(
+        Comment,
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_date = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=24)
