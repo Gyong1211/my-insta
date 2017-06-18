28f2d2fa8f8cf347998eda24acf5aa2d7571e83c
Gihong Kim <gyong1211@gmail.com>
2017-06-18 20:27:32 +0900

    login, logout 기능 추가(urls, views, templates)

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, login as django_login, logout as django_logout, authenticate
+from django.shortcuts import render, redirect
 
-# Create your views here.
+User = get_user_model()
+
+
+def login(request):
+    if request.method == 'POST':
+        print(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username, password)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            django_login(request, user)
+            return redirect('post:post_list')
+        else:
+            return render(request, 'member/login.html')
+
+    else:
+        return render(request, 'member/login.html')
+
+
+def logout(request):
+    django_logout(request)
+    return redirect('post:post_list')
+
+
+def signup(request):
+    pass
--- django_app/post/views.py
+++ django_app/post/views.py
@@ -6,13 +6,15 @@ from post.models import Post
 def post_list(request):
     posts = Post.objects.all()
     context = {
-        'posts':posts
+        'posts': posts
     }
     return render(request, 'post/post_list.html', context=context)
 
+
 def post_detail(request, post_pk):
-    post = Post.objects.get(id = post_pk)
+    post = Post.objects.get(id=post_pk)
     context = {
-        'post':post,
+        'post': post,
     }
     return render(request, 'post/post_detail.html', context=context)
+
