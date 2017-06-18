e1deffcc0ed39015eb2cdec056261d75028264c5
Gihong Kim <gyong1211@gmail.com>
2017-06-18 18:50:26 +0900

    post_detail 생성(urls, views, templates), post_list에서 post_detail로 가는 링크 추가

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -9,3 +9,10 @@ def post_list(request):
         'posts':posts
     }
     return render(request, 'post/post_list.html', context=context)
+
+def post_detail(request, post_pk):
+    post = Post.objects.get(id = post_pk)
+    context = {
+        'post':post,
+    }
+    return render(request, 'post/post_detail.html', context=context)
