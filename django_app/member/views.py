8f31ef31595cec875c368fba1d38d9b24cece89d
Gihong Kim <gyong1211@gmail.com>
2017-06-21 21:36:39 +0900

    login에 LoginForm 사용, signup 기능 구현

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,24 +1,23 @@
 from django.contrib.auth import get_user_model, login as django_login, logout as django_logout, authenticate
 from django.shortcuts import render, redirect
 
+from .forms import LoginForm, SignupForm
+
 User = get_user_model()
 
 
 def login(request):
     if request.method == 'POST':
-        print(request.POST)
-        username = request.POST['username']
-        password = request.POST['password']
-        print(username, password)
-        user = authenticate(username=username, password=password)
-        if user is not None:
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['user']
             django_login(request, user)
             return redirect('post:post_list')
-        else:
-            return render(request, 'member/login.html')
-
     else:
-        return render(request, 'member/login.html')
+        if request.user.is_authenticated:
+            return redirect('post:post_list')
+
+    return render(request, 'member/login.html')
 
 
 def logout(request):
@@ -26,5 +25,14 @@ def logout(request):
     return redirect('post:post_list')
 
 
+
 def signup(request):
-    pass
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.create_user()
+            django_login(request, user)
+            return redirect('post:post_list')
+    else:
+        pass
+    return render(request, 'member/signup.html')
